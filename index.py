dd35a20666d0eebd63d739683e463f6f8925c037
francisco-neto-fit <francisco.neto@fit-tecnologia.org.br>
2021-08-13 14:14:16 -0300

    feat(requisicao): escolhendo em baixar uma ou todas opcoes

--- index.py
+++ index.py
@@ -1,18 +1,26 @@
 # ======================================= IMPORTS ====================================#
-import requests
+import requests, os
 from dotenv import dotenv_values
 from PyInquirer import prompt
 
 
 # ======================================= VARIABLES ====================================#
 arrayOfChoices = []
-
 config = dotenv_values(".env")
-token = config['TOKEN']
-url = config['URL']
-username = config['USERNAME']
-header = {'Authorization': 'token %s' % token}
+
+if config.__len__() != 0:
+    token = config['TOKEN']
+    url = config['URL']
+    username = config['USERNAME']
+    header = {'Authorization': 'token %s' % token} 
+else:
+    token = '' 
+    username = ''
+    header = {}
+    url = 'https://api.github.com/repos/francisco-neto-fit/apriltag-imgs/contents/'
+
 index = 0
+
 # ======================================= GETTING DATA ====================================#
 # Request to get the tag familys in respository
 
@@ -32,42 +40,72 @@ questions = [
         'choices':arrayOfChoices
     }
 ]
+
+
+
 choice = prompt(questions)
 choice = str(choice['choice'])
 index = int(choice.split(sep='-)')[0])
+familyName = choice.split(sep='-)')[1]
 
 option = arrayOfChoices[index - 1]
 index = 0
 
-response = requests.get(option['url'],headers=header).json()
-arrayOfChoices = []
-
-for data in  response:
-    index += 1
-    option = str(index)+'-)'+ data['name']
-    arrayOfChoices.append({'name':option,'download_url':data['download_url'], 'fileName':data['name']})
-
 questions = [
-        {
-            'type': 'list',
-            'name': 'choice',
-            'message': 'Qual AprilTag você quer?',
-            'choices':arrayOfChoices
-        }
-    ]    
-choice = prompt(questions)['choice']
+    {
+        'type': 'list',
+        'name': 'choice',
+        'message': 'Quantas aprilTAgs deseja baixar?',
+        'choices':['1-)Uma','2-)Todas']
+    }
+]
 
-index = int(choice.split(sep='-)')[0])
+quantityOption = prompt(questions)['choice']
 
-option = arrayOfChoices[index - 1]
+if quantityOption.find('2') != -1:
+    response = requests.get(option['url'],headers=header).json()
+    arrayOfChoices = []
+    os.mkdir(path=familyName)
+    for data in  response:
+        index += 1
+        
+        img = requests.get(data['download_url'], headers=header)
+        path = familyName+'/'+data['name']
+        if data['name'].find('tag') != -1:
+            file = open(path, "wb")
+            file.write(img.content)
+            file.close()  
+            print('✅Arquivo: '+ path + 'criado')  
+else:
+    response = requests.get(option['url'],headers=header).json()
+    arrayOfChoices = []
 
-img = requests.get(option['download_url'], headers=header)
-path = "aprilTags/"+option['fileName']
-file = open(path, "wb")
-file.write(img.content)
-file.close()
+    for data in  response:
+        index += 1
+        option = str(index)+'-)'+ data['name']
+        arrayOfChoices.append({'name':option,'download_url':data['download_url'], 'fileName':data['name']})
 
-print('Script executado com sucesso!')
+    questions = [
+            {
+                'type': 'list',
+                'name': 'choice',
+                'message': 'Qual AprilTag você quer?',
+                'choices':arrayOfChoices
+            }
+        ]    
+    choice = prompt(questions)['choice']
 
+    index = int(choice.split(sep='-)')[0])
 
+    option = arrayOfChoices[index - 1]
+
+    img = requests.get(option['download_url'], headers=header)
+    path = familyName+'/'+option['fileName']
+    os.mkdir(path=familyName)
+    file = open(path, "wb")
+    file.write(img.content)
+    file.close()
+
+print('Script executado com sucesso!✅')
+print('¯\_(ツ)_/¯')
 
